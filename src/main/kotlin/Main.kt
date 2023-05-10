b4b5d938cd73366d660a86b685466a8987cd3248
A.Glazyrin <alex_glazyrin@yahoo.com>
2023-05-10 17:36:44 +0300

    10052023

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,34 @@
 const val HERO_NAME = "Madrigal"
+var playerLevel = 5
 fun main(args: Array<String>) {
     println("The hero announces her presence to the world.")
 
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
+    readBountyBoard()
 
-    val hasBefriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
+    playerLevel += 1
+    println(playerLevel)
+    readBountyBoard()
+}
+
+private fun readBountyBoard(){
+    println("The hero approach the bounty board. It reads:")
+    println(obtainQuest(playerLevel,"paladin",true, false))
+}
+
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String,
+    hasBefriendedBarbarians: Boolean,
+    hasAngeredBarbarians: Boolean
+): String {
     val quest: String = when (playerLevel) {
         1 -> "Meet Mr. Bubbles in the land of soft things."
         in 2..5 -> {
-            //new branch
             val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
             if (canTalkToBarbarians) {
                 "Convince the barbarians to call off their invasion."
@@ -25,13 +41,5 @@ fun main(args: Array<String>) {
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There are no quests right now."
     }
-
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
-
-    println("Time passes...")
-    println("The hero returns from her quest.")
-
-    playerLevel += 1
-    println(playerLevel)
+    return quest
 }
